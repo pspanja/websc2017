5a611abdfc35f8f8a46113eefc24fdcdb4362397
Petar Španja <petar@spanja.info>
2017-08-28 16:59:37 +0200

    Step 7 start

---
+++ src/EzSolrWorkshopBundle/Core/FacetBuilderVisitor/CustomFieldFacetBuilderVisitor.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace EzSolrWorkshopBundle\Core\FacetBuilderVisitor;
+
+use EzSystems\EzPlatformSolrSearchEngine\Query\FacetBuilderVisitor;
+use EzSystems\EzPlatformSolrSearchEngine\Query\FacetFieldVisitor;
+use eZ\Publish\API\Repository\Values\Content\Query\FacetBuilder;
+use EzSolrWorkshopBundle\API\Facet\CustomFieldFacet;
+use EzSolrWorkshopBundle\API\FacetBuilder\CustomFieldFacetBuilder;
+
+/**
+ * Visits the CustomField facet builder.
+ */
+class CustomFieldFacetBuilderVisitor extends FacetBuilderVisitor implements FacetFieldVisitor
+{
+    public function canVisit(FacetBuilder $facetBuilder)
+    {
+        return $facetBuilder instanceof CustomFieldFacetBuilder;
+    }
+
+    /**
+     * Returns facet sort parameter.
+     *
+     * @param \EzSolrWorkshopBundle\API\FacetBuilder\CustomFieldFacetBuilder $facetBuilder
+     *
+     * @return string
+     */
+    private function getSort(CustomFieldFacetBuilder $facetBuilder)
+    {
+        switch ($facetBuilder->sort) {
+            case CustomFieldFacetBuilder::COUNT_DESC:
+                return 'count';
+            case CustomFieldFacetBuilder::TERM_ASC:
+                return 'index';
+        }
+
+        return 'index';
+    }
+
+    public function mapField($field, array $data, FacetBuilder $facetBuilder)
+    {
+        return new CustomFieldFacet(
+            [
+                'name' => $facetBuilder->name,
+                'entries' => $this->mapData($data),
+            ]
+        );
+    }
+
+    public function visitBuilder(FacetBuilder $facetBuilder, $fieldId)
+    {
+        // TODO: return Solr representation of the $facetBuilder
+    }
+}
